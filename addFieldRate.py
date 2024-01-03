00eb4591cb854e866708ca6c6da5d88ad6be5be5
kohai-fred <frederick.vu.pro@gmail.com>
2024-01-03 09:45:44 +0100

    update: add function

--- addFieldRate.py
+++ addFieldRate.py
@@ -1,6 +1,7 @@
 from random import randint
 from population import populations
 
-for population in populations :
-  population["range"] = randint(1, 100)
-print(populations)
+def addFieldRange() :
+  for population in populations :
+    population["range"] = randint(1, 100)
+  return populations
